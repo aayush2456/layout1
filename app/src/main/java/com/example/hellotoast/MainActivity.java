98bfdb02e24afc8ca73c17df7468aa730f8b8fc4
aayush2456 <jaayush21@tbc.edu.np>
2021-12-09 22:49:25 +0545

    Code Implemented

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button countButton;
+    private Button toastButton;
+    private TextView textView;
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        countButton= findViewById(R.id.button_count);
+        toastButton= findViewById(R.id.button_toast);
+        textView= findViewById(R.id.count_text);
+        toastButton.setOnClickListener(this);
+        countButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_toast){
+            Toast.makeText(MainActivity.this,"Clicked",Toast.LENGTH_LONG).show();
+        }else if(v.getId() == R.id.button_count){
+            count++;
+            Log.d("MainActivity",""+count);
+            textView.setText(""+count);
+        }
+
     }
 }
